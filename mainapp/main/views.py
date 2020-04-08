bddd398d9ce1c223888d511e99eac1d08168c32b
ddusi <58924238+ddusi@users.noreply.github.com>
2020-04-08 20:57:03 +0900

    로그인세션 버그 fix

--- mainapp/main/views.py
+++ mainapp/main/views.py
@@ -72,10 +72,43 @@ def signin(request):
         request.session['user_id'] = member.user_id
         request.session['member_id'] = member.member_id
 
-        try:
-            if member:
-                return redirect('/swipe/')
-                
-        except Member.DoesNotExist:
-            return redirect('/main/signin/')
+        
+        if member:
+            return redirect('/swipe/')
+        else:
+            return redirect('/main')
+   
+
   
+
+#   # 두번째 시도
+# def signin(request):
+#     if request.method == "GET":
+#         return render(request, 'mainapp/signin.html')
+#         # 잘못된 접근입니다. 알림뜨게 하기
+    
+#     elif request.method == "POST":
+#         # 전송받은 아이디, 비밀번호 확인
+#         user_id = request.POST.get('user_id')
+#         user_pw = request.POST.get('user_pw')
+        
+
+#         #유효성 처리
+#         res_data = {}
+#         if not (user_id and user_pw):
+#             res_data['error']="모든 칸을 다 입력해주세요."
+#         else:
+#             # 기존 DB에 있는 member 모델과 같은 값인 걸 가져옴.
+#             member = Member.objects.get(user_id = user_id, user_pw=user_pw)
+
+#             # 비밀번호가 맞는지 확인한다. 위에 check_password를 참조
+#             if check_password(password, member.user_pw):
+#                 #응답 데이터 세션에 값 추가. 수신측 쿠키에 저장된다.
+#                 request.session['user_id'] = member.user_id
+                
+#                 #리다이렉트
+#                 return redirect('/')
+#             else:
+#                 res_data['error'] = "비밀번호가 틀렸습니다."
+#         return render(request, 'signin.html', res_data)
+#                 #응답 데이터 res_data 전달
